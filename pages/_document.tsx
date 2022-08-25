b77015e19623e7b6cc2b5ca0c9a04ef44213a90b
qwerty <arsen.abzaev@mail.ru>
2022-08-25 14:33:13 +0500

    5

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,14 +17,12 @@ export default function Document() {
         ></script>
       </Head>
       <body>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=GTM-${GTM}`}
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-${GTM}"
+height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        ></noscript>
         <Main />
         <NextScript />
       </body>
