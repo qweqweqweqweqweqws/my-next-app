dfd35b27ec5ef8c970c1530816beb39652c6fffe
user11 <user11@user11-desktop.spectrum.int>
2023-02-28 16:16:49 +0500

    Редирект www

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [{ type: 'host', value: `www.${process.env.CHECKPERSON_PUBLIC_SITE}.com` }],
+        destination: `https://${process.env.NEXT_PUBLIC_URL}.com/:path*`,
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
