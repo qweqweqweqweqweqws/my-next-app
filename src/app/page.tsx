ce55db5af28fd369586689bb7c4e8608decfa5fb
Арсен Абзаев <abzaev.a@avtocod.ru>
2023-12-05 16:38:57 +0500

    temp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,23 @@
 import Image from 'next/image'
 import useExperiment from '../../hooks/useExperiment';
 import Link from 'next/link';
-import Second from './second';
-import First from './first';
+import Second from './second/page';
+import First from './first/page';
 
 export default function Home() {
-  const variant = useExperiment("i2Ri3Ga8SoKm_p1maUngGA");
-  console.log(variant);
-  console.log(typeof variant);
+  // const variant = useExperiment("i2Ri3Ga8SoKm_p1maUngGA");
+  // console.log(variant);
+  // console.log(typeof variant);
 
-  if (variant === "1") {
-    return <First />;
-  }
+  // if (variant === "1") {
+  //   return <First />;
+  // }
 
-  if (variant === "2") {
-    return <Second />;
-  }
+  // if (variant === "2") {
+  //   return <Second />;
+  // }
+
+  console.log('11')
 
   return (
     <main>
