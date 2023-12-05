1473b89ae9e7d723f4e2f995cfe571813f5f810b
Арсен Абзаев <abzaev.a@avtocod.ru>
2023-12-05 17:52:38 +0500

    1

--- src/app/first/page.tsx
+++ src/app/first/page.tsx
@@ -2,7 +2,77 @@ import React from "react";
 import styles from './index.module.css'
 
 const First = () => {
-  return <div className={styles['container']}>111FIRST</div>;
+  return <div className={styles['container']}>111FIRST
+  <a href="https://apps.apple.com/ru/app/spectrumdata/id1529224299">app store</a>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  <p>111FIRST</p>
+  </div>;
 };
 
 export default First;
